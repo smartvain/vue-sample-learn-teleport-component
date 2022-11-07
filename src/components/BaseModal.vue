d98f31431c674a7e4c2e7d18c3b7e24aad766985
smartvain <zlc.blue15210@gmail.com>
2022-11-07 15:40:12 +0900

    add: sample component with using teleport component

---
+++ src/components/BaseModal.vue
@@ -0,0 +1,32 @@
+<template>
+  <button @click="openModal">View User</button>
+
+  <Teleport to="#modal">
+    <div class="modal-wrap" v-if="isModalOpen">
+      <div class="modal">
+        <slot>
+          <h1>Vue Teleport</h1>
+          <p>This modal is now outside of our vue application!</p>
+        </slot>
+        <button @click="closeModal">Close</button>
+      </div>
+    </div>
+  </Teleport>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const isModalOpen = ref(false)
+
+const openModal = () => {
+  isModalOpen.value = true
+}
+
+const closeModal = () => {
+  isModalOpen.value = false
+}
+</script>
+
+<style scoped lang="scss">
+</style>
---
+++ src/components/UserCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="user-card">
+    <p>John Komarnicki</p>
+    <span>Front-End Developer</span>
+    <BaseModal />
+  </div>
+</template>
+
+<script setup lang="ts">
+import BaseModal from './BaseModal.vue'
+</script>
+
+<style scoped lang="scss">
+.user-card {
+  padding: 20px;
+  background-color: rgb(0, 55, 65);
+  border-radius: 4px;
+
+  p {
+    font-size: 24px;
+    margin-bottom: 8px;
+  }
+}
+</style>
